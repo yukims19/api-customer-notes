dcd5f3d36bdc75c90614e77890daf8937015bc6e
Youxi Li <yol107@ucsd.edu>
2018-08-20 19:33:19 -0700

    Update notes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").load();
 const express = require("express");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 const escape = require("pg-escape");
 const fetch = require("node-fetch");
@@ -16,6 +18,30 @@ app.get("/customers", (req, res) => {
   client.query(sql, (error, response) => {
     resData = response.rows;
     res.send(resData);
+  });
+});
+
+app.get("/customers/:id", (req, res) => {
+  const sql = escape("SELECT * FROM customers WHERE id=%L", req.params.id);
+  let resData;
+  client.query(sql, (error, response) => {
+    resData = response.rows;
+    res.send(resData);
+  });
+});
+
+app.post("/save/:field", (req, res) => {
+  const field = req.params.field;
+  const fieldValue = req.body[field];
+  const customerId = req.body.id;
+  const sql = escape(
+    "UPDATE customers SET %s=%L WHERE id=%L;",
+    field,
+    fieldValue,
+    customerId.toString()
+  );
+  client.query(sql, (error, response) => {
+    console.log(error, response);
   });
 });
 
