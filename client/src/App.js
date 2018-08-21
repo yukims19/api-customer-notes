23bd1faf2c7a8bb75c5211cb8554d19af6106fb7
Youxi Li <yol107@ucsd.edu>
2018-08-20 19:34:17 -0700

    Update and Get customer notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Collapse, Input } from "antd";
+import { Collapse, Input, Button } from "antd";
 const Panel = Collapse.Panel;
 
 class Filter extends Component {
@@ -9,13 +9,6 @@ class Filter extends Component {
       <div className="left-filter">
         <Collapse defaultActiveKey={["1"]}>
           <Panel header="Filter" key="1">
-            {/*<input
-              id="post-filter"
-              type="text"
-              placeholder="Filter By Name"
-              ref="postfilter"
-              onChange={this.props.onChange}
-            />*/}
             <Input
               placeholder="Enter Customer Name"
               onChange={this.props.onChange}
@@ -28,13 +21,134 @@ class Filter extends Component {
 }
 
 class Customer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      invoice: this.props.selectedCustomer
+        ? this.props.selectedCustomer.invoice
+        : "",
+      password: this.props.selectedCustomer
+        ? this.props.selectedCustomer.password
+        : "",
+      others: this.props.selectedCustomer
+        ? this.props.selectedCustomer.others
+        : ""
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      JSON.stringify(this.props.selectedCustomer) !==
+      JSON.stringify(prevProps.selectedCustomer)
+    ) {
+      console.log("not mathcing");
+      this.setState(
+        {
+          invoice: this.props.selectedCustomer.invoice
+            ? this.props.selectedCustomer.invoice
+            : "",
+          password: this.props.selectedCustomer.password
+            ? this.props.selectedCustomer.password
+            : "",
+          others: this.props.selectedCustomer.others
+            ? this.props.selectedCustomer.others
+            : ""
+        },
+        () => {
+          console.log(this.state.invoice);
+        }
+      );
+    }
+  }
+
+  handleSave(field, customerId) {
+    this.callSave(field, customerId).catch(err => console.log(err));
+  }
+  callSave = async (field, customerId) => {
+    let content = null;
+    switch (field) {
+      case "invoice":
+        content = { invoice: this.invoice.value, id: customerId };
+        break;
+      case "password":
+        content = { password: this.password.value, id: customerId };
+        break;
+      case "others":
+        content = { others: this.others.value, id: customerId };
+        break;
+      default:
+        console.log("Wrong");
+    }
+    const response = await fetch("/save/" + field, {
+      method: "POST",
+      body: JSON.stringify(content),
+      headers: { "Content-Type": "application/json" }
+    }).then(res => res);
+
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+  handleTextareaChange(event, field) {
+    this.setState({ [field]: event.target.value });
+  }
   render() {
     return (
       <div className="customer-main">
         {this.props.selectedCustomer
-          ? <h1>
-              {this.props.selectedCustomer.name}
-            </h1>
+          ? <div>
+              {" "}<h1>{this.props.selectedCustomer.name}</h1>
+              <Collapse bordered={false} defaultActiveKey={["1"]}>
+                <Panel header="Invoice" key="1">
+                  <textarea
+                    ref={textarea => (this.invoice = textarea)}
+                    value={this.state.invoice}
+                    onChange={e => this.handleTextareaChange(e, "invoice")}
+                  />
+                  <Button
+                    type="primary"
+                    onClick={() =>
+                      this.handleSave(
+                        "invoice",
+                        this.props.selectedCustomer.id
+                      )}
+                  >
+                    Save
+                  </Button>
+                </Panel>
+                <Panel header="Password" key="2">
+                  <textarea
+                    ref={textarea => (this.password = textarea)}
+                    value={this.state.password}
+                    onChange={e => this.handleTextareaChange(e, "password")}
+                  />
+                  <Button
+                    type="primary"
+                    onClick={() =>
+                      this.handleSave(
+                        "password",
+                        this.props.selectedCustomer.id
+                      )}
+                  >
+                    Save
+                  </Button>
+                </Panel>
+                <Panel header="Others" key="3">
+                  <textarea
+                    ref={textarea => (this.others = textarea)}
+                    value={this.state.others}
+                    onChange={e => this.handleTextareaChange(e, "others")}
+                  />
+                  <Button
+                    type="primary"
+                    onClick={() =>
+                      this.handleSave("others", this.props.selectedCustomer.id)}
+                  >
+                    Save
+                  </Button>
+                </Panel>
+              </Collapse>
+            </div>
           : ""}
       </div>
     );
@@ -42,34 +156,15 @@ class Customer extends Component {
 }
 
 class AllCustomers extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      customersAll: []
-    };
-  }
   componentDidMount() {
-    this.callCustomers()
-      .then(res =>
-        this.setState({
-          customersAll: res
-        })
-      )
-      .catch(err => console.log(err));
+    this.props.getAllCustomers();
   }
-
-  callCustomers = async () => {
-    const response = await fetch("/customers");
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
 
   render() {
     return (
       <div className="left-body">
-        {this.state.customersAll
-          ? this.state.customersAll.map(customer => {
+        {this.props.customersAll
+          ? this.props.customersAll.map(customer => {
               if (
                 this.props.filter == null ||
                 customer.name
@@ -104,17 +199,47 @@ class App extends Component {
     super(props);
     this.state = {
       filter: null,
-      selectedCustomer: null
+      selectedCustomer: null,
+      customersAll: []
     };
   }
+  getAllCustomers() {
+    this.callCustomers()
+      .then(res =>
+        this.setState({
+          customersAll: res
+        })
+      )
+      .catch(err => console.log(err));
+  }
+  callCustomers = async () => {
+    const response = await fetch("/customers");
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
   onChange(event) {
     this.setState({ filter: event.target.value });
   }
+
   handleCustomerSelection(customer) {
-    console.log("selected");
-    console.log(customer);
-    this.setState({ selectedCustomer: customer });
+    this.callSelectedCustomers(customer.id)
+      .then(res => {
+        this.setState({
+          selectedCustomer: res[0]
+        });
+      })
+      .catch(err => console.log(err));
   }
+
+  callSelectedCustomers = async id => {
+    const response = await fetch("/customers/" + id);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -123,6 +248,8 @@ class App extends Component {
           <AllCustomers
             handleCustomerSelection={this.handleCustomerSelection.bind(this)}
             filter={this.state.filter}
+            getAllCustomers={this.getAllCustomers.bind(this)}
+            customersAll={this.state.customersAll}
           />
         </div>
 
