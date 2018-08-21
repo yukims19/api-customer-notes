3ba6625267df5e7644fe8e81ecf9c5b136b481b7
Youxi Li <yol107@ucsd.edu>
2018-08-21 16:48:14 -0700

    Fix bugs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //require("dotenv").load();
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
